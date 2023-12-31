4e79aa76704b5f26659d34a96f230ce0c2ae98de
Aakash Dixit <aakash26dixit@gmail.com>
2023-12-31 11:35:47 +0530

    first commit

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use(cors());
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: `http://localhost:${process.env.PORT}`,
     methods: ['GET', 'POST'],
   },
 });
